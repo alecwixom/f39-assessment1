2c4f89b4a5d8a0faf9874913e6855670f756aabe
Alec Wixom <atwixom@gmail.com>
2023-04-14 15:13:51 -0600

    Updated with comments

--- orchard.js
+++ orchard.js
@@ -50,6 +50,9 @@ const pinkPrice = .55
 */
 
 // CODE HERE
+
+// In this for loop I decided to make the totalAcres = the math from adding the other acres up giving the total value over to totalAcres.
+
 let totalAcres = 0
 
 for (i = 0; i < fujiAcres.length; i++){
@@ -72,6 +75,9 @@ console.log(totalAcres);
 */
 
 // CODE HERE
+
+// In this code, I decided to make the averageDailyAcres = the total divided by the fuji's length. The reason being if the fuji length gets longer it will still give the correct average.
+
 let averageDailyAcres = totalAcres / fujiAcres.length
 console.log(averageDailyAcres);
 
@@ -111,6 +117,11 @@ let acresLeft = 174
 let days = 0
 
 // CODE HERE
+
+/* In the while loop, it will continue to run as long as the value of acresLeft is above 0.
+The value of days increases by 1 every time the loop goes through and the value of acresLeft goes down by the average (9 i believe)
+*/
+
 while (acresLeft > 0){
     days++
     acresLeft -= averageDailyAcres
@@ -142,6 +153,9 @@ console.log(days);
 */
 
 // CODE HERE
+
+// This for loop basically takes the type of apples in tons and gives it the value of that type's acres * acreTon. This gives you the apples ton value.
+
 let fujiTons = []
 let galaTons = []
 let pinkTons = []
@@ -175,6 +189,11 @@ console.log(pinkTons)
 */
 
 // CODE HERE 
+
+/*
+The for loop runs depending on the length of fuji' acres array so that it constantly updates with new value's.
+The apple type's lps value is calculated on the tons * lbs giving appletypePounds that value.
+*/
 
 let fujiPounds = []
 let galaPounds = []
@@ -210,6 +229,11 @@ console.log(pinkPounds)
 
 // CODE HERE
 
+/*
+There are three for loops that run based on the legth of the lbs list, then calculating the appletypePounds * applePrice to give the appleProfit.
+I could have made this simpler but this is the only way it made sense in my head.
+*/
+
 let fujiProfit = []
 let galaProfit = []
 let pinkProfit = []
@@ -243,6 +267,14 @@ console.log(pinkProfit);
 */
 
 // CODE HERE
+
+/*
+Now for this one, I got stuck for a long time.
+Eventually looked up a method using .reduce which combines the values set in array together and then add those three together to give a total profit.
+The acc is for the accumulator and the cc is the current value, in otherwords the acc and cur are going to add up all the arrays,
+then add the other arrays together to get a totalProfit value.
+*/
+
 console.log('----------------------------------');
 
 let totalProfit = fujiProfit.reduce((acc, cur) => acc + cur) + galaProfit.reduce((acc, cur) => acc + cur) + pinkProfit.reduce((acc, cur) => acc + cur)
